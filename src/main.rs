0915347faae5e92384766a85fe8e77c75ef73928
Vabka <vab-999@ya.ru>
2021-12-01 16:09:07 +0500

    ---

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,14 @@ fn solve_day_1() {
     let result3 = solve_day_1_part_2_windowless(&measurements);
     println!("Alternative result of part 2: {result3}");
 }
+// Window 2. Subwindow 1
+// Window 4. Subwindo
 fn solve_day_1_part_1(puzzle_input: &Vec<i32>) -> usize {
-    get_growth_count(puzzle_input)
+    get_growth_count(puzzle_input.into_iter())
 }
 fn solve_day_1_part_2(puzzle_input: &Vec<i32>) -> usize {
-    let moving_sum: Vec<i32> = puzzle_input
-        .windows(3)
-        .map(|wnd| wnd.iter().sum())
-        .collect();
-    get_growth_count(&moving_sum)
+    let iter = puzzle_input.windows(3).map(|wnd| wnd.iter().sum::<i32>());
+    get_growth_count(iter)
 }
 
 fn solve_day_1_part_2_windowless(puzzle_input: &Vec<i32>) -> u32 {
@@ -43,14 +42,12 @@ fn solve_day_1_part_2_windowless(puzzle_input: &Vec<i32>) -> u32 {
     increases
 }
 
-fn get_growth_count<T: Clone + PartialOrd, Collection: IntoIterator<Item = T>>(
-    measurements: Collection,
-) -> usize {
-    measurements
-        .into_iter()
-        .tuple_windows()
-        .filter(|(p, n)| p < n)
-        .count()
+fn get_growth_count<T, Collection>(iterator: Collection) -> usize
+where
+    T: Clone + PartialOrd,
+    Collection: Iterator<Item = T>,
+{
+    iterator.tuple_windows().filter(|(p, n)| p < n).count()
 }
 
 fn get_raw_measurements() -> Vec<i32> {
